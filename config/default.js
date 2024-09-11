a152dad24c84e4584c9457f7087e5752f88e8919
x1 <108907332+rxc0n@users.noreply.github.com>
2024-09-11 00:58:46 -0400

    Create default.js

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+    TOR_PROXY: 'socks5h://127.0.0.1:9050',
+    RENTRY_BASE_URL: 'https://rentry.co',
+    USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    PASTE_CONTENT: 'This is a default paste content.',
+    CUSTOM_URL: ''
+};
